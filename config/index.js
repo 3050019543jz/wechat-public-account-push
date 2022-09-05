da9f87d940f6abdc7df567196694830a6d8f1dbb
3050019543jz <112862674+3050019543jz@users.noreply.github.com>
2022-09-05 18:40:28 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -39,6 +39,7 @@ export const config = {
       city: "无锡",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://www.bilibili.com"
+      }
     
     
   ],
